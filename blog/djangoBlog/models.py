55415cd952b27d0544252c5c7379b3e8674ba3fc
Bartosz Kędziora <kedziora.bartosz@gmail.com>
2017-02-12 19:02:09 +0100

    Adds models

--- blog/djangoBlog/models.py
+++ blog/djangoBlog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Blog(models.Model):
+    user = models.OneToOneField(User, related_name="account")
+    header = models.ImageField(default='blank_header.jpg')
+    about = models.TextField(null=True, blank=True)
+
+
+class Tag(models.Model):
+    value = models.CharField(max_length=100)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    publish_date = models.DateField(default=timezone.now)
+    tags = models.ManyToManyField(Tag)
+    blog = models.ForeignKey(Blog)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+
+class View(models.Model):
+    view_date = models.DateTimeField(default=timezone.now)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
